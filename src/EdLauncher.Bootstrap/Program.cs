b0390a4cfe48672caca8ef0d070cb35f6163e11c
Chris <github@nsaas.io>
2020-11-24 20:17:37 -0700

    add a separate launcher since epic doesn't allow changing launch exe

---
+++ src/EdLauncher.Bootstrap/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace EdLauncher.Bootstrap
+{
+    // C# instead of F# since final exe output is quite a bit smaller
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "EdLauncher.exe",
+                Arguments = string.Join(" ", args),
+                CreateNoWindow = false,
+                UseShellExecute = false
+            };
+
+            var p = Process.Start(startInfo);
+            p?.WaitForExit();
+        }
+    }
+}
