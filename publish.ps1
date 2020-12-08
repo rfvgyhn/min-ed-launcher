faf6618f3b49186b91b79790e732abaf29c272ef
Chris <github@nsaas.io>
2020-12-07 21:01:28 -0700

    fix not restoring packages based on rid

--- publish.ps1
+++ publish.ps1
@@ -5,8 +5,9 @@ $target="win10-x64"
 $version=$proj.Project.PropertyGroup.VersionPrefix
 $release_name="min-ed-launcher_v${version}_$target"
 
-dotnet publish src\MinEdLauncher\MinEdLauncher.fsproj -r "$target" --self-contained true -o "artifacts\$release_name" -c ReleaseWindows -p:PublishSingleFile=true
-dotnet publish src\MinEdLauncher.Bootstrap\MinEdLauncher.Bootstrap.csproj -r "$target" --self-contained true -o "artifacts\$release_name" -c Release
+dotnet restore -r $target
+dotnet publish src\MinEdLauncher\MinEdLauncher.fsproj -r "$target" --self-contained true --no-restore -o "artifacts\$release_name" -c ReleaseWindows -p:PublishSingleFile=true
+dotnet publish src\MinEdLauncher.Bootstrap\MinEdLauncher.Bootstrap.csproj -r "$target" --self-contained true --no-restore -o "artifacts\$release_name" -c Release
 cp README.md "artifacts\$release_name"
 rm "artifacts\$release_name\*" -include *.json, *.pdb
 
--- publish.sh
+++ publish.sh
@@ -4,7 +4,8 @@ target="linux-x64"
 version=$(grep -oPm1 "(?<=<VersionPrefix>)[^<]+" src/Directory.Build.props) # use something like xml_grep if this regex becomes a problem
 release_name="min-ed-launcher_v${version}_$target"
 
-dotnet publish src/MinEdLauncher/MinEdLauncher.fsproj -r "$target" --self-contained true -o "artifacts/$release_name" -c Release -p:PublishSingleFile=true
+dotnet restore -r $target
+dotnet publish src/MinEdLauncher/MinEdLauncher.fsproj -r "$target" --self-contained true --no-restore -o "artifacts/$release_name" -c Release -p:PublishSingleFile=true
 cp README.md "artifacts/$release_name"
 rm artifacts/"$release_name"/*.pdb
 
