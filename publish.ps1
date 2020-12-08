f886c69f7c4c9ec66a636db9fcf9ea94c4d83c5a
Chris <github@nsaas.io>
2020-12-07 20:59:40 -0700

    provide better label for release artifacts

--- publish.ps1
+++ publish.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 $target="win10-x64"
 [xml]$proj = Get-Content src\Directory.Build.props
 $version=$proj.Project.PropertyGroup.VersionPrefix
-$release_name="min-ed-launcher-$version-$target"
+$release_name="min-ed-launcher_v${version}_$target"
 
 dotnet publish src\MinEdLauncher\MinEdLauncher.fsproj -r "$target" --self-contained true -o "artifacts\$release_name" -c ReleaseWindows -p:PublishSingleFile=true
 dotnet publish src\MinEdLauncher.Bootstrap\MinEdLauncher.Bootstrap.csproj -r "$target" --self-contained true -o "artifacts\$release_name" -c Release
--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 target="linux-x64"
 version=$(grep -oPm1 "(?<=<VersionPrefix>)[^<]+" src/Directory.Build.props) # use something like xml_grep if this regex becomes a problem
-release_name="min-ed-launcher-$version-$target"
+release_name="min-ed-launcher_v${version}_$target"
 
 dotnet publish src/MinEdLauncher/MinEdLauncher.fsproj -r "$target" --self-contained true -o "artifacts/$release_name" -c Release -p:PublishSingleFile=true
 cp README.md "artifacts/$release_name"
