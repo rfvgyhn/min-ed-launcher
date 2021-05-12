90f69bcc54dc968a71e0c718d7aaa95a3fcbab08
Chris <github@nsaas.io>
2021-05-11 21:45:29 -0600

    include changelog in release artifacts

--- publish.ps1
+++ publish.ps1
@@ -8,7 +8,7 @@ $release_name="min-ed-launcher_v${version}_$target"
 dotnet restore -r $target
 dotnet publish src\MinEdLauncher\MinEdLauncher.fsproj -r "$target" --self-contained true --no-restore -o "artifacts\$release_name" -c ReleaseWindows -p:PublishSingleFile=true
 dotnet publish src\MinEdLauncher.Bootstrap\MinEdLauncher.Bootstrap.csproj -r "$target" --self-contained true --no-restore -o "artifacts\$release_name" -c Release
-cp README.md "artifacts\$release_name"
+cp README.md CHANGELOG.md "artifacts\$release_name"
 rm "artifacts\$release_name\*" -include *.json, *.pdb
 
 Compress-Archive -Path "artifacts\$release_name" -DestinationPath "artifacts\$release_name.zip" -Force
--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ release_name="min-ed-launcher_v${version}_$target"
 
 dotnet restore -r $target
 dotnet publish src/MinEdLauncher/MinEdLauncher.fsproj -r "$target" --self-contained true --no-restore -o "artifacts/$release_name" -c Release -p:PublishSingleFile=true
-cp README.md "artifacts/$release_name"
+cp README.md CHANGELOG.md "artifacts/$release_name"
 rm artifacts/"$release_name"/*.pdb
 
 tar czvf "artifacts/$release_name.tar.gz" -C "artifacts" "$release_name"
