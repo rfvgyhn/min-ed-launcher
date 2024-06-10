8a83c7591faf266de16e9c67fd7219cd400b990e
Chris <github@nsaas.io>
2024-06-09 22:14:51 -0600

    fix cargo fmt

--- build.rs
+++ build.rs
@@ -3,10 +3,10 @@ use winresource::WindowsResource;
 
 fn main() {
     let icon_path = "resources/min-ed-launcher.ico";
-    
+
     if Path::new(icon_path).is_file() {
         let mut res = WindowsResource::new();
-        
+
         res.set_icon(icon_path);
         res.compile().expect("failed to build executable icon");
     }
