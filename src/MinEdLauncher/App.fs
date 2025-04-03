rfvgyhn/min-ed-launcher

commit d50b0f86bfcdf915a781d7892fb4bf0362a7978e

asynchronously read processes' stdout/err. seems to fix issue with running edomh. possibly related to #90

src/MinEdLauncher/Process.fs
@@ -7,7 +7,10 @@ let launchProcesses (processes:ProcessStartInfo list) =
     processes
     |> List.choose (fun p ->
         try
-            Process.Start(p) |> Some
+            let p = Process.Start(p)
+            p.BeginErrorReadLine()
+            p.BeginOutputReadLine()
+            p |> Some
         with
         | :? Win32Exception as e ->
             Log.exn e $"""Unable to start process %s{p.FileName}
@@ -29,8 +32,6 @@ let stopProcesses timeout (processes: Process list) =
             Log.debug $"Stopping process %s{p.ProcessName}"
             match Interop.termProcess timeout p with
             | Ok () ->
-                p.StandardOutput.ReadToEnd() |> ignore
-                p.StandardError.ReadToEnd() |> ignore
                 Log.info $"Stopped process %s{p.ProcessName}"
             | Error msg -> Log.warn msg)
     

commit b31db0e218b76b5626e09e59dfe0cb270c4fcee2

fix trying to do async read multiple times on external processes
regression from d50b0f86bfcdf915a781d7892fb4bf0362a7978e

fixes #156

src/MinEdLauncher/App.fs
@@ -331,8 +331,8 @@ let rec private launchLoop initialLaunch settings playableProducts (session: EdS
         if not initialLaunch then
             do! renewEpicTokenIfNeeded settings.Platform session.PlatformToken
         
-        let persistentProcesses = persistentRunning |> Option.defaultWith (fun () -> Process.launchProcesses persistentStartInfos)
-        let mutable relaunchProcesses = Process.launchProcesses relaunchStartInfos
+        let persistentProcesses = persistentRunning |> Option.defaultWith (fun () -> Process.launchProcesses false persistentStartInfos)
+        let mutable relaunchProcesses = Process.launchProcesses false relaunchStartInfos
         
         if initialLaunch && settings.GameStartDelay > TimeSpan.Zero then
             Log.info $"Delaying game launch for %.2f{settings.GameStartDelay.TotalSeconds} seconds"
@@ -363,7 +363,7 @@ let rec private launchLoop initialLaunch settings playableProducts (session: EdS
             while settings.Restart.IsSome && not (Console.cancelRestart timeout) do
                 Process.stopProcesses settings.ShutdownTimeout relaunchProcesses
                 logStart relaunchStartInfos
-                relaunchProcesses <- Process.launchProcesses relaunchStartInfos
+                relaunchProcesses <- Process.launchProcesses false relaunchStartInfos
                 
                 do! renewEpicTokenIfNeeded settings.Platform session.PlatformToken
                 
@@ -557,7 +557,7 @@ let run settings launcherVersion cancellationToken = taskResult {
         
     Process.stopProcesses settings.ShutdownTimeout processesToStop
     settings.ShutdownProcesses |> List.iter (fun p -> Log.info $"Starting process %s{p.FileName}")
-    Process.launchProcesses settings.ShutdownProcesses |> Process.writeOutput
+    Process.launchProcesses true settings.ShutdownProcesses |> Process.waitForExit
     
     return didLoop
 } 

src/MinEdLauncher/Process.fs
@@ -3,13 +3,18 @@ module MinEdLauncher.Process
 open System.ComponentModel
 open System.Diagnostics
 
-let launchProcesses (processes:ProcessStartInfo list) =
+let launchProcesses printOutput (processes:ProcessStartInfo list) =
     processes
     |> List.choose (fun p ->
         try
             let p = Process.Start(p)
             p.BeginErrorReadLine()
             p.BeginOutputReadLine()
+            
+            if printOutput then
+                p.OutputDataReceived.Add(fun a -> if a.Data <> null then printfn $"  %s{a.Data}")
+                p.ErrorDataReceived.Add(fun a -> if a.Data <> null then printfn $"  %s{a.Data}")
+                
             p |> Some
         with
         | :? Win32Exception as e ->
@@ -35,14 +40,9 @@ let stopProcesses timeout (processes: Process list) =
                 Log.info $"Stopped process %s{p.ProcessName}"
             | Error msg -> Log.warn msg)
     
-let writeOutput (processes: Process list) =
+let waitForExit (processes: Process list) =
     processes
     |> List.iter(fun p ->
         use p = p
-        p.EnableRaisingEvents <- true
-        p.OutputDataReceived.Add(fun a -> if a.Data <> null then printfn $"  %s{a.Data}")
-        p.ErrorDataReceived.Add(fun a -> if a.Data <> null then printfn $"  %s{a.Data}")
-        p.BeginErrorReadLine()
-        p.BeginOutputReadLine()
         p.WaitForExit()
     )